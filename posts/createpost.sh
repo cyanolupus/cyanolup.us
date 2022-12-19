1357b087868eb3cc4f64069e8ce8eb2e7c9ab645
cyanolupus <cyanolupus@gmail.com>
2022-12-19 20:32:57 +0900

    sh

---
+++ posts/createpost.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cwd=$(pwd)
+cd $1
+
+header="<!doctype html>
+<html>
+<head>
+<meta charset='UTF-8'><meta name='viewport' content='width=device-width initial-scale=1'>
+<meta name='viewport' content='width=device-width,initial-scale=1.0'>
+<link rel='shortcut icon' href='https://cyanolup.us/img/icon_trs_low.png' type='image/x-icon'>
+<meta property='og:type' content='website'>
+<meta property='og:url' content='cyanolup.us'>
+<meta property='“og:image”' content='https://cyanolup.us/img/icon.jpg'>
+<meta name='twitter:card' content='summary'>
+<meta name='twitter:site' content='@cyanolupus'>
+<meta name='“twitter:image”' content='https://cyanolup.us/img/icon.jpg'>
+<link rel='stylesheet' href='https://cyanolup.us/css/main.css' type='text/css'>
+<link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/github-dark.min.css'>
+<link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js'>
+<meta property='og:description' content='さいあの/るぷす'>
+<meta property='og:title' content='$(basename `pwd`)'>
+<title>$(basename `pwd`)</title></head>"
+
+footer="<script src='https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js'></script>
+<script>hljs.initHighlightingOnLoad();</script></body>
+</html>"
+
+pandocres="$(pandoc index.md)"
+
+echo "$header$pandocres$footer" > index.html
+
+cd $cwd
